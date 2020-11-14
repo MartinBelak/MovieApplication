70e20c804a3c7f9836d7461b58f7a9774372bf1b
MartinBelak <martin.belak140@gmail.com>
2020-11-14 15:58:59 +0100

    Update HomeController.cs

--- MovieApiServer/Controllers/HomeController.cs
+++ MovieApiServer/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using MoviesAPIServer.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
 
 namespace MoviesAPIServer.Controllers
 {
@@ -10,7 +13,28 @@ namespace MoviesAPIServer.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            var Movie = new MovieModel();
+            string connectionString = @"Server=tcp:uniqeservername.database.windows.net,1433;Initial Catalog=MoviesDB;User Id=AdminLogin;Password=AdminPassword123;";
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                var query = "Select * from [dbo].Movie";
+                              
+                SqlCommand comand = new SqlCommand(query, conn);
+                SqlDataReader reader = comand.ExecuteReader();
+                while(reader.Read())
+                {
+                    Movie.Title = reader["Title"].ToString();
+                    Movie.Plot = reader["Plot"].ToString(); 
+                }
+
+
+                conn.Close();
+
+                ViewBag.Title = "Home Page";
+
+            }
 
             return View();
         }
