3aa9c307029d6aebfe3ef8e676bb7cda0d77fa1d
MartinBelak <martin.belak140@gmail.com>
2020-11-20 09:44:35 +0100

    ...

---
+++ MovieClient/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieClient.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
