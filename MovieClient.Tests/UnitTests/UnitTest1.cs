a4594336f5e5023863381c5418e6614bbe29136d
MartinBelak <martin.belak140@gmail.com>
2021-01-07 18:53:41 +0100

    Unit Tests

---
+++ MovieClient.Tests/UnitTests/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovieClient.Controllers;
+using MovieClient.Models;
+using MovieClient.Persistency;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDatabaseConnection()
+        {
+            //arrange           
+            //act
+            var instance = AzureDb.Instance;
+            //assert
+            Assert.IsNotNull(instance);
+        }
+        [TestMethod]
+        public void TestReceiveMovies()
+        {
+            //arrange            
+            //act
+            var movies = new MovieController().AllMovies();
+            //assert
+            Assert.IsNotNull(movies);
+        }
+        [TestMethod]
+        public void TestMovieDetails()
+        {
+            //arrange
+            MovieModel model = new MovieModel();
+            var controller = new MovieController();
+            //act
+            var MovieDetailsView = controller.MovieDetails(model);
+            //assert
+            Assert.IsInstanceOfType(MovieDetailsView, typeof(ActionResult));
+        }
+    }
+}
